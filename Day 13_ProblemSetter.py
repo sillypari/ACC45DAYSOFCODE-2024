c2ebbbb4839040cb31a373b426cf0ae60de9c075
sillypari <138761938+sillypari@users.noreply.github.com>
2024-10-10 21:07:53 +0530

    Day 13 - Problem Setter

---
+++ Day 13_ProblemSetter.py
@@ -0,0 +1,9 @@
+# https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/EXPERT
+
+X=int(input("Enter no. of problems submitted : "))
+Y=int(input("Enter no. of problems approved : "))
+
+if (Y/X>=0.5):
+    print("Yes! expert")
+else : 
+    print("Not! Expert")
